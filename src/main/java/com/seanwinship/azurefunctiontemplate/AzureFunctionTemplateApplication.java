1888f7feac2f1407f8a2818edac702bacc45c0ab
Sean Winship <winship.sean@gmail.com>
2024-12-16 02:12:07 +0000

    Upgrade to 3.4.0 Spring Boot Parent

--- src/main/java/com/seanwinship/azurefunctiontemplate/AzureFunctionTemplateApplication.java
+++ src/main/java/com/seanwinship/azurefunctiontemplate/AzureFunctionTemplateApplication.java
@@ -2,10 +2,8 @@ package com.seanwinship.azurefunctiontemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
 public class AzureFunctionTemplateApplication {
 
     public static void main(String[] args) {
